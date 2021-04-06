fa106ea4479ed59187bd02656c77e501caff3c88
Ivan Solenkov <ivan_solenkov1999@abv.bg>
2021-04-06 15:31:50 +0300

    Contact Us added

--- Lorem Taxi/src/Components/Contact Us/Contact.jsx
+++ Lorem Taxi/src/Components/Contact Us/Contact.jsx
@@ -1,7 +1,27 @@
+import taxiImg from '../img/about-img.png'
+
 const Contact = () => {
     return (
         <div className="contact">
-            <h2>Contact Us</h2>
+            <div className="contact-heading">
+                <h2>
+                    Any Problems
+                    <br />
+                    Any Questions
+                </h2>
+            </div>
+            <div className="contact-container">
+                <div className="contact-box">
+                    <h4>Get In touch</h4>
+                    <form action="/">
+                        <input type="text" placeholder="Name" />
+                        <input type="number" placeholder="Phone Number" />
+                        <input type="text" placeholder="Message" id="input-msg" />
+                        <button>Send</button>
+                    </form>
+                </div>
+                <img src={taxiImg} alt="taxi-img" />
+            </div>
         </div>
     );
 }

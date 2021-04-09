52cdaa85ee297bb85d0044e4af76548a7086b200
Ivan Solenkov <ivan_solenkov1999@abv.bg>
2021-04-09 16:18:58 +0300

    Full Responsive Design Portrait added

--- Lorem Taxi/src/Components/Navigation.jsx
+++ Lorem Taxi/src/Components/Navigation.jsx
@@ -1,9 +1,26 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Navigation = () => {
+    let timesClicked = 0;
+
+    const burgerButton = (e) => {
+        e.preventDefault();
+        timesClicked++;
+
+        if (timesClicked % 2 === 0) {
+            document.querySelector('.navigation ul').style.display = "none";
+            document.querySelector('.navigation').style.height = "8vh";
+        } else {
+            document.querySelector('.navigation ul').style.display = "flex";
+            document.querySelector('.navigation').style.height = "40vh";
+        }
+    }
     return (
         <div className="navigation">
             <h3><Link to="/">LoremTaxi</Link></h3>
+            <a href="true" onClick={burgerButton} id="burger-menu"><FontAwesomeIcon icon={faBars} /></a>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
